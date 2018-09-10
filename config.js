ba3a5630b0f15c45cc08f40bc45e4efa40396882
Cecille Salazar <cecillersalazar@gmail.com>
2018-09-10 09:03:08 -0700

    Set JWT_SECRET and JWT_EXPIRY in config.js

--- config.js
+++ config.js
@@ -7,5 +7,7 @@ module.exports = {
         process.env.DATABASE_URL || 'mongodb://testuser:testuser1@ds155278.mlab.com:55278/countries-users',
   TEST_DATABASE_URL:
         process.env.TEST_DATABASE_URL ||
-        'mongodb://localhost/thinkful-backend-test'
+        'mongodb://localhost/thinkful-backend-test',
+  JWT_SECRET: 'miko',
+  JWT_EXPIRY: '7d'
 };
