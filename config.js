c8a520cf452da7ae761b1763efea0bdde4a8f713
Cecille Salazar <cecillersalazar@gmail.com>
2018-09-10 08:40:46 -0700

    Set DATABASE_URL with mlab link

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   PORT: process.env.PORT || 8080,
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
   DATABASE_URL:
-        process.env.DATABASE_URL || 'mongodb://localhost/thinkful-backend',
+        process.env.DATABASE_URL || 'mongodb://testuser:testuser1@ds155278.mlab.com:55278/countries-users',
   TEST_DATABASE_URL:
         process.env.TEST_DATABASE_URL ||
         'mongodb://localhost/thinkful-backend-test'
