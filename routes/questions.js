83b481037aff3bb1c500c2d4e406e7d109a8e50c
MorganHuegel <mhue2910@gmail.com>
2018-09-12 22:51:45 -0400

    updated /api/questions endpoint so that it will generate a new filteredList for the user if the filteredList is empty

--- routes/questions.js
+++ routes/questions.js
@@ -9,6 +9,8 @@ const { jwtStrategy } = require('../auth/strategies');
 const User = require('../mongo_schema/user-schema.js');
 const Question = require('../mongo_schema/question-data-schema.js');
 
+const {handleAnswer, generateNewQuestions} = require('../linked-list');
+
 const router = express.Router();
 router.use(express.json());
 router.use(passport.authenticate('jwt', {session: false, failWithError: true}));
@@ -16,7 +18,25 @@ router.use(passport.authenticate('jwt', {session: false, failWithError: true}));
 router.get('/', (req, res, next) => {
   const username = req.user.username;
   User.find({username})
-    .then(questionUrl => res.json(questionUrl[0].filteredList[0].url))
+    .then(userData => {
+      //If the user's question list is not empty, send back the first quesetion
+      if(userData[0].filteredList.length > 0){
+        res.json(userData[0].filteredList[0].url);
+      } 
+      //If the user's question list IS empty, we need to regenerate questions before sending back the first question
+      else {
+        return Question.find().then(allQuestions => {
+          const newQuestionList = generateNewQuestions(allQuestions, userData[0].questionLevels);
+          return User.findOneAndUpdate(
+            {username}, 
+            {filteredList: newQuestionList}, 
+            {new: true}
+          ).then(newUserData => {
+            res.json(newUserData.filteredList[0].url);
+          });
+        });
+      }
+    })
     .catch(err => next(err));
 });
 
@@ -30,13 +50,13 @@ router.post('/', (req, res, next) => {
     .then(country => {
       if(country.toLowerCase().trim() === answer.toLowerCase().trim()) {
         isCorrect = true;
-        return res.json({isCorrect, country})
+        return res.json({isCorrect, country});
       } else {
         isCorrect = false;
-        return res.json({isCorrect, country})
+        return res.json({isCorrect, country});
       }
     })
-    .catch(err => next(err))
-})
+    .catch(err => next(err));
+});
 
 module.exports = router;
