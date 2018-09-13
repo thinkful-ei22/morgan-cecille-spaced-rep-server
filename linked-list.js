73fbedd058360b25bba9e361e6790a4c8275ad5f
Cecille Salazar <cecillersalazar@gmail.com>
2018-09-13 11:30:03 -0700

    Fixed bug in implementing new data structure - fully functional

--- linked-list.js
+++ linked-list.js
@@ -1,45 +1,12 @@
 'use strict';
 
-// const questions = [
-//   {
-//     id: 1111,
-//     url: 'foo.com',
-//     country: 'mexico'
-//   },
-//   {
-//     id: 2222,
-//     url: 'baz.com',
-//     country: 'america'
-//   },
-//   {
-//     id: 3333,
-//     url: 'bar.com',
-//     country: 'finland'
-//   }
-// ]
-//
-// const filteredQuestions = [
-//   {
-//     id: 1111,
-//     url: 'foo.com',
-//     country: 'mexico'
-//   },
-//   {
-//     id: 2222,
-//     url: 'baz.com',
-//     country: 'america'
-//   },
-// ]
-//
-// const questionLevels = [1, 1, 2];
-
 const generateNewQuestions = function(questions, questionLevels, startIndex = 0) {
   let newArray = [];
   //We're trying to find the lowest values in the question level array and push them to newArray
   let lowestLevel = 5;
   for(let i = 0; i < questionLevels.length; i++) {
     if(questionLevels[i].level < lowestLevel) {
-      lowestLevel = questionLevels[i];
+      lowestLevel = questionLevels[i].level;
     }
   }
   //Now that we've found the lowest level - which is 1 - we need to loop through the
@@ -73,7 +40,5 @@ const handleAnswer = function(questions, userData, isCorrect) {
   userData.filteredList.shift();
   return userData;
 };
-
-// handleAnswer();
 
 module.exports = {handleAnswer, generateNewQuestions};
